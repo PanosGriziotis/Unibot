8d48a89f3c22f97fff5964b1a6eb73c2209d2e6f
PanosGriziotis <72866911+PanosGriziotis@users.noreply.github.com>
2022-02-14 23:05:07 +0200

    change language to el

--- Unibot/actions/actions.py
+++ Unibot/actions/actions.py
@@ -1,65 +1,105 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
-
-
-# This is a simple example for a custom action which utters "Hello World!"
+# This files contains custom actions
 
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SlotSet
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from Levenshtein import distance
 from . import database
 
-class ActionListCourses(Action):
+
+class ValidateCourseForm(FormValidationAction):
+    def __init__(self):
+        self.target_num_semester = None
+        self.target_course_name = None
 
     def name(self) -> Text:
-        return "action_list_courses"
+        return "validate_course_form"
+
+    def validate_num_semester(
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        # Validate `num_semester` value.
+
+        allowed_semesters_list = database.get_semesters_list()
+
+        #  test if given slot value aligns with db
+
+        for string in allowed_semesters_list:
+            if slot_value in string:
+                self.target_num_semester = string
+            else:
+                self.target_course_name = None
+
+        # validation message
+
+        if self.target_num_semester is None:
+            dispatcher.utter_message(
+                text=f"Σόρρυ. Δεν υπάρχει {slot_value} εξάμηνο. Τα διαθέσιμα εξάμηνα του τμήματος είναι τα παρακάτω: {','.join(allowed_semesters_list)}.")
+            return {"num_semester": None}
+        dispatcher.utter_message(text=f"Τέλεια! Ψάχνεις, λοιπόν, για ένα μάθημα για το {self.target_num_semester}.")
+        return {"num_semester": self.target_num_semester}
+
+    def validate_course_name(
+            self,
+            slot_value: Any,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        # Validate `course_name` value.
+
+        semester_name = tracker.get_slot('num_semester')
+        allowed_courses_names = database.get_courses_list_per_semester(semester_name)
+
+        # test if given slot value aligns with db (Lev distance)
+
+        distances = {}
+        for course_name in allowed_courses_names:
+            distances[course_name] = distance(course_name, str(slot_value))
+        print (distances)
+        for course_name, distance_value in distances.items():
+            print (course_name)
+            print (distance_value)
+            if distance_value <= 5:
+                self.target_course_name = course_name
+                break
+            if slot_value in course_name:
+                self.target_course_name = course_name
+                
+        print ('after computing distance:' , self.target_course_name)
+        # validation message
+
+        if self.target_course_name is None:
+            dispatcher.utter_message(
+                text=f"Χμμ. Δυσκολεύομαι να αναγνωρίσω αυτό το μάθημα. Για να σε βοηθήσω, ορίστε μία λίστα με τα μαθήματα του εξαμήνου: {' ,'.join(allowed_courses_names)}.")
+            return {"course_name": self.target_course_name}
+        dispatcher.utter_message(text=f"Τέλεια! Ενδιαφέρεσαι για το μάθημα {self.target_course_name}.")
+        return {"course_name": self.target_course_name} 
+ 
+ 
+
+class ActionSupplyCourseDescription(Action):
+
+    def name(self) -> Text:
+        return "action_supply_course_description"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        semester = next(tracker.get_latest_entity_values("semester"),None)
 
-        # Test if given entity aligns to nlu data
-        if not semester or semester == None:
-            msg = f"You said: {semester}.I am sorry, I did not get that."
-            dispatcher.utter_message(text=msg)
-            return []
+        
+        # get description from db
 
-        # return the output of relevant method
-        msg = database.get_courses_per_semester(semester)
-        dispatcher.utter_message(text=msg)
+        description = database.get_course_description(tracker.get_slot("course_name"), tracker.get_slot("num_semester"))
 
+        # message
+
+        dispatcher.utter_message(
+            text=f'Το περιεχόμενο του μαθήματος {tracker.get_slot("course_name")} αφορά σε γενικές γραμμές τα εξής: {description} ')
         return []
-
-class ActionGiveCourseInfo(Action):
-
-    def name(self) -> Text:
-        return "action_give_course_info"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        course_name = next(tracker.get_latest_entity_values("course"), None)
-        semester = tracker.get_slot("semester")
-
-        # Test if given semester entity aligns to nlu data
-        if not semester or semester == None:
-            msg = f"You said: {semester}.I am sorry, I did not get that."
-            dispatcher.utter_message(text=msg)
-            return []
-
-        # Test if given course entity aligns to nlu data
-        if not course_name or course_name == None:
-            msg = f"You said {course_name}. I am sorry, I did not get that."
-            dispatcher.utter_message(text=msg)
-            return []
-
-        msg = database.get_course_information(course_name,semester)
-        dispatcher.utter_message(text=msg)
-
-        return[]
--- Unibot/actions/database.py
+++ Unibot/actions/database.py
@@ -1,14 +1,25 @@
+# This file contains the data retrieval code using web scrapping
+
+from matplotlib.pyplot import get
 import requests
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import pandas as  pd
 import re
+import numpy as np
 
 URL_MAIN = 'https://www.di.uoa.gr'
-URL_COURSES = "https://www.di.uoa.gr/en/studies/undergraduate/courses"
+URL_COURSES = "https://www.di.uoa.gr/studies/undergraduate/courses"
+URL_WINTER_19 = 'https://www.di.uoa.gr/schedule/19-20/timetable_PPS_winter1920.html'
+URL_SPRING_19 = 'https://www.di.uoa.gr/schedule/19-20/timetable_PPS_spring1920.html'
 
-def get_all_courses(URL):
+def parse_page (url):
+    page = requests.get(url)
+    return BeautifulSoup(page.content,'html.parser')
+
+def get_all_courses(url):
     all_courses_dict = {}
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
+    soup = parse_page(url)
     all_semesters_tables = soup.find_all('table', class_="table table-striped cols-10")
     for table_class in all_semesters_tables:
         semester = table_class.find('caption').get_text().strip()
@@ -22,46 +33,125 @@ def get_all_courses(URL):
 
     return all_courses_dict
 
-def get_courses_per_semester(semester_name):
 
+def get_semesters_list():
+    all_courses_dict = get_all_courses(URL_COURSES)
+    return list(all_courses_dict.keys())
+
+
+def get_courses_list_per_semester(semester_name):
     all_courses_dict = get_all_courses(URL_COURSES)
     if str(semester_name) in all_courses_dict.keys():
         courses = all_courses_dict.get(semester_name)
-        courses_names = '\n'.join(list(courses.keys()))
-        return f'{semester_name}\n\n{courses_names}'
-    else:
-        return f"I didn't recognize {semester_name}. Is it spelled correctly?"
+        courses_names = list(courses.keys())
 
-def get_course_information (course_name,semester_name):
+        return courses_names
+    else:
+        return None
+
+
+def get_course_description(course_name, semester_name):
     all_courses_dict = get_all_courses(URL_COURSES)
     courses = all_courses_dict.get(semester_name)
     if str(course_name) in courses.keys():
         url = courses.get(course_name)
-    else:
-        return f"I didn't understand {semester_name}. Is it spelled correctly?"
-    page = requests.get(URL_MAIN+url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find(id="block-corporate-lite-content")
-    content = results.find_all('div', class_="views-field views-field-body")
-    for tag in content:
-        text = tag.find('p').text
-        if text != '':
-            return f'{semester_name}\t\tCourse: {course_name}\n\n {text}'
+        soup = parse_page(URL_MAIN + url)
+        results = soup.find(id="block-corporate-lite-content")
+        content = results.find_all('div', class_="views-field views-field-body")
+        for tag in content:
+            text = tag.find('p').text
+            if text != '':
+                return text
 
-def normalise_semester_entity (semester_entity):
-    word2ordinal = {'first':'1st','second':'2nd','third':'3rd','fourth':'4th','fifth':'5th','sixth':'6th','seventh':'7th','eighth':'8th'}
-    num2ordinal = {'1':'1st','2':'2nd','3':'3rd','4':'4th','5':'5th','6':'6th','7':'7th','8':'8th'}
+def get_schedules(url1,url2):
+    dfs = []
 
-    token_list = re.findall(r'\b(?!(?i)semester\b)[\w\d]+',semester_entity) # leave out 'semester'
+    for i in [url1,url2]:
 
-    for token in token_list:
-        if token.lower() in word2ordinal.keys(): # e.g. first
-            ordinal = word2ordinal[token]
-        if token.lower() in num2ordinal.values(): # e.g. 1st
-            ordinal = token
-        if token in num2ordinal.keys(): # e.g. 1
-            ordinal = num2ordinal[token]
+        soup = parse_page(i)
+        schedule_rows = []
+        table = soup.find('table')
+        rows = soup.find_all('tr')
+        global day
 
-            return ordinal
+        for row in rows:
+            row_dict = {}
+            column = 0
+            row_dict = {}
+            tds = row.find_all('td')
 
-print (get_course_information('Linear Algebra','Semester: 1st'))
+            if len(tds) == 1:
+                day = tds[0].find('font').get_text(strip=True)
+
+            for td in tds:
+                content = td.find('font').get_text(strip=True,separator = '\t')
+                row_dict[column] = content
+                column+=1
+                row_dict['day'] = day
+            
+            schedule_rows.append(row_dict)
+        df = pd.DataFrame(schedule_rows)
+        dfs.append(df)
+    n_df = pd.concat(dfs)
+    n_df.columns = n_df.iloc[1]
+    n_df.rename(columns={'Δευτέρα':'day','Ώρα/Αίθ.':'time'}, inplace = True)
+
+    return n_df
+
+def get_schedule_dict_for_course (course_name):
+    
+    schedule_df = get_schedules(URL_WINTER_19,URL_SPRING_19)
+        
+    mask = np.column_stack([schedule_df[col].str.contains(rf'{course_name}', na=False) for col in schedule_df])
+    answer = schedule_df.loc[mask.any(axis=1)]
+
+    aulas = []
+    for col in answer:
+        mask = answer[col].str.contains(rf'{course_name}', na=False)
+        if mask.any() == True:
+            aulas.append(col)
+    indeces = answer.reset_index(drop=True).index
+    
+    exact_values = []
+    for idx in indeces:
+        for aula in aulas:
+            exact_value = answer.iloc[idx][aula]
+            if course_name in exact_value:
+                exact_values.append(exact_value)
+    unique_values = list(set(exact_values))
+
+    schedule_dict = {} 
+    for col in aulas:          
+        for value in unique_values:
+            course_dict = {}
+            time = ', '.join(list(answer['time'].where(answer[col] == value).dropna().values))
+            day = ','.join(list(set(list(answer['day'].where(answer[col] == value).dropna().values))))
+            if len(time) > 0  and len(day) > 0:
+                course_dict['professor'] = value.split('\t')[2]
+                course_dict['time'] = time
+                course_dict['day'] = day
+                course_dict['aula'] = col
+                course_key = value.split('\t')[0]
+            
+                if course_key in schedule_dict.keys():
+                    for k,old_dict in schedule_dict.items():
+                        dicts = [old_dict,course_dict]
+                        new_dict = {}
+                        for key in old_dict:
+                            new_dict[key] = tuple(d[key]for d in dicts)
+                        
+                        schedule_dict[course_key] = new_dict
+                else:
+                    schedule_dict[course_key] = course_dict
+    return schedule_dict
+
+def message(course_name):
+    schedule_dict = get_schedule_dict_for_course(course_name)
+    for course, dic in schedule_dict.items():
+        for key in dic:
+            if type(dic[key]) == tuple:
+                dic[key] = ' και '.join(map(str,dic[key]))
+        
+        print(f'To μάθημα {course} διδάσκεται την/τις ημέρα/-ες {dic["day"]} με τον/τους καθηγητή/-ές {dic["professor"]} και τις ώρες {dic["time"]}  στην/στις αίθουσα/-ες: {dic["aula"]}')
+
+
